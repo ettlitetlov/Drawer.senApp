441fa772f3ebf7a78f3cb5bcf718049152923fd2
yrsa <yrsa.lifvergren@gmail.com>
2015-11-26 14:04:00 +0100

    Revert "Kanske funkar med gotoGroup-knapp"
    This reverts commit 6cf33fb2f05d964ce4a3f5f53d4b5e893e2c74e3.

--- app/src/main/java/com/example/axel/drawer/GroupFragment.java
+++ app/src/main/java/com/example/axel/drawer/GroupFragment.java
@@ -4,12 +4,9 @@ package com.example.axel.drawer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;   //tillagd, men det hjälpte ju inte
 import android.view.ViewGroup;
-import android.widget.Button;
 
 
 /**
@@ -27,25 +24,7 @@ public class GroupFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_group, container, false);
-
-        //testar lite, här under är det tillagt
-        View view = inflater.inflate(R.layout.fragment_group, container, false);    //vad som ska visas?
-        Button addGroupButton = (Button) view.findViewById(R.id.skapaGrupp);                //säg att knappen är skapaGrupp-knappen
-        addGroupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                // Så, jag tror att vi ska skicka vidare användaren till nästa fragmenten (addGroup) här?
-                AddGroupFragment fragment = new AddGroupFragment();
-                android.support.v4.app.FragmentTransaction fragmentTransaction =
-                        getActivity().getSupportFragmentManager().beginTransaction(); //getActivity() tillagt innan getSupportF…
-                fragmentTransaction.replace(R.id.fragment_container, fragment);
-                fragmentTransaction.commit();
-
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_group, container, false);
     }
 
 }
