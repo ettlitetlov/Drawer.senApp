dbee5bdce865126c2581cef4e38c024c999f2302
Skankalicious <bollbergo@gmail.com>
2015-11-26 14:31:48 +0100

    Fungerande navigation mellan fragments

--- app/src/main/java/com/example/axel/drawer/GroupFragment.java
+++ app/src/main/java/com/example/axel/drawer/GroupFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -19,12 +20,36 @@ public class GroupFragment extends Fragment {
         // Required empty public constructor
     }
 
+   /* @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group, container, false);
+    } */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group, container, false);
+        //return inflater.inflate(R.layout.fragment_group, container, false);
+
+        //testar lite, här under är det tillagt
+        View view = inflater.inflate(R.layout.fragment_group, container, false);    //vad som ska visas?
+        Button addGroupButton = (Button) view.findViewById(R.id.skapaGrupp);                //säg att knappen är skapaGrupp-knappen
+        addGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // Så, jag tror att vi ska skicka vidare användaren till nästa fragmenten (addGroup) här?
+                AddGroupFragment fragment = new AddGroupFragment();
+                android.support.v4.app.FragmentTransaction fragmentTransaction =
+                        getActivity().getSupportFragmentManager().beginTransaction(); //getActivity() tillagt innan getSupportF…
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.commit();
+
+            }
+        });
+        return view;
     }
 
 }
